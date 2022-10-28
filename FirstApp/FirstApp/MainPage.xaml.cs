9ac04242292da68258d7f5dbead9837cd55630e4
Lynn Simonen <lynnsimonen@yahoo.com>
2022-10-27 19:15:03 -0500

    Add project files.

---
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+      
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            myOutput.Text = $"You clicked {count} times.";
+        }
+    }
+}
